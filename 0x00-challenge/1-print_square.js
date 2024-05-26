92c917ba48bac85adc3d440d43946a34099ffc51
mdbentaleb <bentalebm@outlook.com>
2024-05-26 21:29:46 +0100

    -

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 /*
-    Print a square with the character #
-    
-    The size of the square must be the first argument 
-    of the program.
+	Print a square with the character #
+
+	The size of the square must be the first argument 
+	of the program.
 */
 
 
